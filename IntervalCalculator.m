9dee8df9a07648cd6040bffa3ab446da06347746
Eden Parish <parish.ethan.p@gmail.com>
2024-08-02 12:07:55 -0600

    Interval calculator algorithm (NOT WORKING!)

--- IntervalCalculator.m
+++ IntervalCalculator.m
@@ -2,6 +2,8 @@ clc, clearvars, close all
 
 syms x;
 
+steps = 3;
+
 f1(x) = 2*x;
 f2(x) = 2*x-1;
 
@@ -15,4 +17,39 @@ for i = 1:degree
     G{i} = finverse(F{i});
 end
 
+OneStepIntervals = [degree];
+
+for i = 1:degree
+    OneStepIntervals(i) = G{i}(0);
+end
+
+IntervalAmount = 0;
+
+for i = 1:steps
+    IntervalAmount = IntervalAmount + degree^i;
+end
+
+Intervals = cat(2,OneStepIntervals, zeros(1,IntervalAmount-degree));
+
+row = 2;
+movementsInRow = 0;
+
+for index = (degree+1):IntervalAmount
+    nestedPoint = LeftMostIndexOfRow(degree,row-1)+mod(movementsInRow,degree^(row-1));
+
+    correspondingInverse = G{RootParent(degree,index,row)}
+
+    Intervals(index) = correspondingInverse(nestedPoint);
+
+    Point = [index, Intervals(index)]
+
+    movementsInRow = movementsInRow + 1;
+    
+    if movementsInRow >= degree^row 
+        movementsInRow = 0;
+        row = row +1;
+    end
+end
+
+Intervals
 
