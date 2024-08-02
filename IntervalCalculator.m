0c57db9421feb9b59b5f3bdc267755dc9ecf8228
Eden Parish <parish.ethan.p@gmail.com>
2024-08-02 11:52:46 -0600

    Fixed a missing +1 in LeftmostIndexOfRow. Found inverses and degree of F.

--- IntervalCalculator.m
+++ IntervalCalculator.m
@@ -1,3 +1,18 @@
 clc, clearvars, close all
 
 syms x;
+
+f1(x) = 2*x;
+f2(x) = 2*x-1;
+
+F = {f1, f2};
+degree = size(F,2);
+
+G = {degree};
+
+%Calculate inverses.
+for i = 1:degree
+    G{i} = finverse(F{i});
+end
+
+
--- LeftMostIndexOfRow.m
+++ LeftMostIndexOfRow.m
@@ -7,6 +7,6 @@ for i = 1:row-1
     rowLengths = rowLengths + degree^i;
 end
 
-outIndex = rowLengths;
+outIndex = rowLengths + 1;
 end
 
