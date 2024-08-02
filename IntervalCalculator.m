d0c449a6323cc8d6a470062fdbd55541af6d4edd
Eden Parish <parish.ethan.p@gmail.com>
2024-08-02 13:01:04 -0600

    Interval Calculator.

--- IntervalCalculator.m
+++ IntervalCalculator.m
@@ -2,23 +2,23 @@ clc, clearvars, close all
 
 syms x;
 
-steps = 10;
+steps = 12;
 
-epsilon = 7;
+epsilon = pi;
 
 f1(x) = (2+epsilon)*x;
 f2(x) = ((2+epsilon)*x-1)/(1+epsilon);
 
 F = {f1, f2};
 
-IntervalsF = cat(2,GetIntervals(F,steps),[1]);
+IntervalsF = cat(2,GetIntervals(F,steps),1);
 
 e1(x) = 2*x;
 e2(x) = 2*x-1;
 
 E = {e1, e2};
 
-IntervalsE = cat(2,GetIntervals(E,steps),[1]);
+IntervalsE = cat(2,GetIntervals(E,steps),1);
 
-plot(IntervalsF,IntervalsE,'-o');
+plot(IntervalsF,IntervalsE,'-o','MarkerSize',1);
 
